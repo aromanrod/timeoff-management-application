75744775919fc51bb0ce81dd5595ddf78493b281
Armando Alberto Roman Rodriguez <armando.roman@globant.com>
2020-04-01 17:37:09 -0500

    improving deployment vars and structure

--- Infrastructure/startweb.ps1
+++
@@ -1,7 +0,0 @@
-Connect-AzAccount
-$resourcegroupname = "timeoff-management-rg"
-$appservicename = "timeoff-management-app"
-$getwebapp = Get-AzWebApp -ResourceGroupName $resourcegroupname -Name $appservicename
-
-npm install sqlite3
-setx /m Sqlite "D:\home\site\wwwroot\Sqlite>"
